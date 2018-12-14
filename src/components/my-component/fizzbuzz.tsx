5eeab987dcbe52535d13cee8d6ad537b52193bba
James Steinbach <jdsteinbach@gmail.com>
2018-12-14 15:22:02 -0700

    set fizz & buzz real good on default

--- src/components/my-component/fizzbuzz.tsx
+++ src/components/my-component/fizzbuzz.tsx
@@ -13,26 +13,30 @@ import {
 export class FizzBuzz {
   @Prop() fizzColor: string = 'blue'
   @Prop() buzzColor: string = 'red'
+  @Prop({mutable: true}) value: number = 0
   @Prop({mutable: true}) fizz: string = '0'
   @Prop({mutable: true}) buzz: string = '0'
-  @Prop({mutable: true}) value: number = 0
   @Prop() maximum: number = 999
 
   @Watch('value')
   valueChange(newValue: number) {
-    console.log(newValue)
-    this.fizz = ( newValue % 3 > 0 )
-              ? '0'
-              : '1'
-    this.buzz = ( newValue % 5 > 0 )
-              ? '0'
-              : '1'
+    this.fizz = this.moduloString(newValue, 3)
+    this.buzz = this.moduloString(newValue, 5)
+  }
+
+  private moduloString(numerator, denominator) {
+    return (numerator % denominator === 0) ? '1' : '0'
   }
 
   incrementValue() {
     if ( this.value < this.maximum ) {
       this.value = this.value + 1
     }
+  }
+
+  componentDidLoad() {
+    this.fizz = this.moduloString(this.value, 3)
+    this.buzz = this.moduloString(this.value, 5)
   }
 
   render() {
